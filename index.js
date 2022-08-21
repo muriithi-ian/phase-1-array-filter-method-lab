c772a6f9aa86765375ddfb0e88716c88f021aee0
Muriithi Ian <imuriithiian@gmail.com>
2022-08-21 12:25:27 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter((driver) => {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter((driver) => {
+    return driver.charAt(0) === name.charAt(0);
+  });
+}
+
+function matchName(drivers, name) {
+  return drivers.filter((driver) => {
+    return driver.name === name;
+  });
+}
+    
